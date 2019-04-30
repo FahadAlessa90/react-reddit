e551ecf5c6935507a6649d5791f9474a3793cdb8
Marc Wright <marc@generalassemb.ly>
2019-04-30 16:35:30 +0300

    trying out bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Reddit />
-    </div>
+    <div class="container">
+      <div class="row">
+        <Reddit />
+      </div>
+    </div>    
   );
 }
 
--- src/Article.js
+++ src/Article.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Article(props) {
   return (
-    <div>
-      <img src={props.article.thumbnail} alt={props.article.title} />
-      <a href={props.article.url} target="_blank" rel="noopener noreferrer">{props.article.title}</a>
+    <div class="col-lg-6">
+      <div class="card">
+        <img src={props.article.thumbnail} class="card-img-top" alt={props.article.title} />
+        <div class="card-body">
+          <h5 class="card-title">{props.article.title}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href={props.article.url} class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     </div>
   );
 }
