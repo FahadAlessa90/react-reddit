a02f97a47f229db2db06a76ce2fcd363ac2aee0b
Marc Wright <marc@generalassemb.ly>
2019-04-30 14:14:11 +0300

    add component did mount and reddit working

--- src/Reddit.js
+++ src/Reddit.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class Reddit extends Component {
+
+  state = {
+    articles: []
+  }
+
+  componentDidMount() {
+    fetch('https://www.reddit.com/hot.json')
+      .then(response => {
+        return response.json()
+      })
+      .then(articles => {
+        console.log(articles.data.children)
+      })
+
+  }
+
   render() {
     return (
       <div>
