aa56107714f9747f1355003512db94d29e9aa4c4
Marc Wright <marc@generalassemb.ly>
2019-04-30 14:04:06 +0300

    Add Reddit component

---
+++ src/Reddit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reddit extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Reddit Feed</h1>
+      </div>
+    )
+  }
+}
+
+export default Reddit
